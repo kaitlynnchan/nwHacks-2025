9163a4883d794d1d02fa7684a4e92242391bca36
Amishi Arora <amishia859@gmail.com>
2025-01-18 18:54:08 -0800

    Added game page

---
+++ frontend/Connect Quest/src/Components/AnswerPanel.jsx
@@ -0,0 +1,8 @@
+export default function AnswerPanel() {
+    return(
+        <div>
+            <button>Enter your answer</button>
+            <button>Snap a pic!</button>
+        </div>
+    )
+}

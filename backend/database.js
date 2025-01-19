466138e64b0d65b1d16fa68502655d8a506088cb
kaitlynnchan <kaitlynnchan@outlook.com>
2025-01-19 10:00:37 -0800

    Update

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/userdb');
+        await mongoose.connect('mongodb+srv://kek:3HscWjrgwTrEXHCR@nwhacks2025.vla9g.mongodb.net');
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
--- backend/models/Challenge.js
+++ backend/models/Challenge.js
@@ -5,6 +5,7 @@ const challengeSchema = new mongoose.Schema({
     description: { type: String, required: true, default: 0 },
     pointsReward: { type: Number, required: true, default: 0.0},
     isActive: { type: Boolean, default: true },
+    createdAt: { type: Date, default: Date.now },
     endDate: { type: Date },
 });
 

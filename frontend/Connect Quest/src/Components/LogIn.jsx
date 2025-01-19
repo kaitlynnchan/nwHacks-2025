bfb4b791a8f3113cdb358c594a4b71a415c0e7e2
Amishi Arora <amishia859@gmail.com>
2025-01-18 17:50:56 -0800

    created log in page

--- frontend/Connect Quest/src/Components/LogIn.jsx
+++ frontend/Connect Quest/src/Components/LogIn.jsx
@@ -2,8 +2,8 @@ import "./LogIn.css";
 export default function LogIn() {
   return (
     <div id="logInDiv">
-      <button>Log in</button>
-      <button>Sign up</button>
+      <button className = "buttons">Log in</button>
+      <button className = "buttons">Sign up</button>
     </div>
   );
 }

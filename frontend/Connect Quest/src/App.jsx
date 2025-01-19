2951f5dc77fccb4861dbea69aa3ecab4ff82e378
Amishi Arora <amishia859@gmail.com>
2025-01-18 18:54:08 -0800

    Added game page

--- frontend/Connect Quest/src/App.jsx
+++ frontend/Connect Quest/src/App.jsx
@@ -1,12 +1,13 @@
-import LogIn from "./components/LogIn.jsx";
+import LogIn from "./Components/LogIn.jsx";
 import logo from "./assets/connect-logo.png"
 import "./index.css";
+
 export default function App() {
   return (
     <div className="App">
       <img id = "logo" src={logo} alt="app logo" />
       <h1 id = "title">Connect Quest</h1>
-      <LogIn />
+      <LogIn/>
     </div>
   );
 }
---
+++ frontend/Connect Quest/src/Components/Challenge.jsx
@@ -0,0 +1,9 @@
+export default function Challenge() {
+    return (
+        <div>
+            <h1>Todays challenge is...</h1>
+            <h1>Ask somebody for their favourite colour</h1>
+            <AnswerPanel></AnswerPanel>
+        </div>
+    ); 
+}

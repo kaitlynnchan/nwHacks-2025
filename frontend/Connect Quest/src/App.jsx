94a8d01941e08e702f2380fc353ff2762ee21208
Amishi Arora <amishia859@gmail.com>
2025-01-18 16:32:23 -0800

    Initial react setup

---
+++ frontend/Connect Quest/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import LogIn from "./Components/LogIn Page/LogIn";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Connect Quest</h1>
+      <LogIn />
+    </div>
+  );
+}
+
---
+++ frontend/Connect Quest/src/Components/LogIn.jsx
@@ -0,0 +1,9 @@
+import "./LogIn.css";
+export default function LogIn() {
+  return (
+    <div id="logInDiv">
+      <button>Log in</button>
+      <button>Sign up</button>
+    </div>
+  );
+}

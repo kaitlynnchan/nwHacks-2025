d4bc41980afbd813e046c5973674551cb43bef6b
ksa239 <kyana_sohangar@sfu.ca>
2025-01-18 18:49:36 -0800

    page router added

--- frontend/Connect Quest/src/App.jsx
+++ frontend/Connect Quest/src/App.jsx
@@ -1,13 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from "./components/LogIn.jsx";
 import logo from "./assets/connect-logo.png"
 import "./index.css";
+import ChallengeList from "./Components/ChallengeList.jsx";
+
 export default function App() {
   return (
-    <div className="App">
-      <img id = "logo" src={logo} alt="app logo" />
-      <h1 id = "title">Connect Quest</h1>
-      <LogIn />
-    </div>
+    <Router>
+      <div className="App">
+        <img id = "logo" src={logo} alt="app logo" />
+        <h1 id = "title">Connect Quest</h1>
+        <Routes>
+          <Route path="/" element={<LogIn />} />
+          <Route path="/challenges" element={<ChallengeList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
